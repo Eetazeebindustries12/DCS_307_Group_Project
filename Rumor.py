198f9bc8445ff8d99955a339dce8056c2b6e46d2
Liam Daly-Smith <104328785+Eetazeebindustries12@users.noreply.github.com>
2023-04-12 12:10:05 -0400

    trying something with rumor propagation

--- Rumor.py
+++ Rumor.py
@@ -3,11 +3,24 @@ import numpy as np
 import random
 class Rumor():
     def __init__(self,level:int,)->None:
+        """_summary_
+
+        Args:
+            level (int): _description_
+            history: list of users that will be filled whenever the rumor propagates, the idea is that 
+        """
         self._history: 'list[User]' = []
         self._level = level
         self._sigmoid = lambda x: 1/(1+np.exp(-x))
-        self._function = lambda a,b: 0.99*np.exp(a*self._sigmoid(b))
+        self._function = lambda a,b: 0.99*np.exp(-a*self._sigmoid(b))
         self._propagation_num:int = 1
-    def push_rumor(self,other:'User'):
+    def push_rumor(self,other:'User',switch: bool = False):
+        if not switch: self._history.append(other)
         headline = random.uniform(0.5,1)
-        
+        believe = self._function(self._propagation_num,self._level)
+        if believe > headline:
+            for i in other._connections:
+                if i not in self._history:
+                    self.push_rumor(i,switch = True)
+if __name__ == "__main__":
+    print("rumor time!")
